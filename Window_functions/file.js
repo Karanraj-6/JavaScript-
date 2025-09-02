7d5b75b953e69fd90ffa19b29a615bf61180a709
Karanraj <karanraj3056@gmail.com>
2025-09-03 00:06:47 +0530

    window functions

---
+++ Window_functions/file.js
@@ -0,0 +1,11 @@
+//* ONLY WORKS IN BROWSER WINDOW
+
+window.alert("hi")  //  shows alert box.
+
+window.setTimeout(fn, 1000) // calls a function after delay.
+
+window.setInterval(fn, 1000) // calls function repeatedly.
+
+window.confirm("are you naughty?") // returns true/false.
+
+window.prompt("type something") // asks input.
